b208681bedaa02496689105bc638e9718c5f1a80
Daniel Zhang <danielzhang95@hotmail.com>
2020-06-23 05:06:06 +0000

    prints error messages from validation

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,15 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Store
+  has_many :employees
+end
+class Employee
+  belongs_to :store
+end
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Daniel", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Daniel", last_name: "Huss", hourly_rate: 60)
+@store1.employees.create(first_name: "Daniel", last_name: "Zhang", hourly_rate: 60)
+@store2.employees.create(first_name: "K", last_name: "V", hourly_rate: 60)
+@store2.employees.create(first_name: "Khu", last_name: "Vi", hourly_rate: 60)
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,20 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, inclusion: { in: (40..200)}
+  validates :store_id, presence: true
+end
+
+class Store
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+end
+
+n = gets.chomp
+st = Store.create(name: n)
+for i in st.errors.messages do
+  puts i
+end
